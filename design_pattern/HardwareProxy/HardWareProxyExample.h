6eec7a9cb69a01ddf618504057a94addf8c3804a
zero <a977644w@126.com>
2016-06-26 11:50:44 +0800

    add dir of design_pattern and uboot_analyze

---
+++ design_pattern/HardwareProxy/HardWareProxyExample.h
@@ -0,0 +1,15 @@
+#ifndef HWProxyExample_H
+#define HWProxyExample_H
+
+struct MotorController;
+struct MotorData;
+struct MotorDispley;
+struct MotorProxy;
+
+typedef enum DirectionType {
+    NO_DIRCTION,
+    FORWARD,
+    REVERSE,
+} DirectionType;
+
+#endif
