7b15a38e194a22a204c5d632aa0c1449bb6e2a5f
zero <a977644w@126.com>
2016-09-18 00:04:15 +0800

    update aplist.lua

--- task/task3/aplist.lua
+++ task/task3/aplist.lua
@@ -1,30 +1,40 @@
 
 local util = require "luci.util"
-local wifi_list_dir = "/tmp/ra0_wifi_list"
+local wifi_list_dir = "ra0_wifi_list"
 local fs = require "luci.fs"
 
 --local ret_str = luci.util.exec("iwpriv ra0 set SiteSurvey=")
 --ret_str = luci.util.exec("iwpriv ra0 get_site_survey")
-ret_str = luci.util.exec("cat wifi_list.txt")
+local ret_str = util.exec("cat wifi_list.txt")
 
 --@ refresh
---util.exec("rm "..wifi_list_dir)
---util.exec("touch "..wifi_list_dir)
+if fs.access(wifi_list_dir) then
+    util.exec("rm "..wifi_list_dir)
+end
+util.exec("touch "..wifi_list_dir)
 local _file = io.open(wifi_list_dir,"w+")
-local channel_regex="([0-9]+)"
-local ssid_regex="([a-zA-Z0-9%.%-%_/]+)
-local bssid_regex="(%w%w:%w%w:%w%w:%w%w:%w%w:%w%w)"
-local security_regex="([a-zA-Z0-9%/]+)"
-local signal_regex="([0-9]+)"
+local ch_reg="([0-9]+)"
+local ss_reg="([a-zA-Z0-9%.%-%_/]+)"
+local bss_reg="(%w%w:%w%w:%w%w:%w%w:%w%w:%w%w)"
+local sec_reg="([a-zA-Z0-9%/]+)"
+local sig_reg="([0-9]+)"
+local wps_reg="(%u+)"
 
 local tmp_tb = luci.util.split(ret_str,"\n")
 for k,v in pairs(tmp_tb or {}) do
 	if v and v:match("^[0-9]") then
-		local wifi_tb = {}
+		local channel = ""
+		local ssid = ""
+		local bssid = ""
+		local security = ""
+		local signal = ""
+        local wps = ""
 
-		wifi_tb.channel,wifi_tb.ssid,wifi_tb.bssid,wifi_tb.security,wifi_tb.signal = v:match("^"%s+([a-zA-Z0-9%.%-%_/]+)%s+(%w%w:%w%w:%w%w:%w%w:%w%w:%w%w)%s+([a-zA-Z0-9%/]+)%s+([0-9]+)%s*") 
-		if wifi_tb.channel then
-			_file:write(wifi_tb.channel..","..wifi_tb.ssid..","..wifi_tb.bssid..","..wifi_tb.security..","..wifi_tb.signal.."\n")
+		--wifi_tb.channel,wifi_tb.ssid,wifi_tb.bssid,wifi_tb.security,wifi_tb.signal = v:match("^"%s+([a-zA-Z0-9%.%-%_/]+)%s+(%w%w:%w%w:%w%w:%w%w:%w%w:%w%w)%s+([a-zA-Z0-9%/]+)%s+([0-9]+)%s*") 
+		channel,ssid,bssid,security,signal,wps = v:match("^"..ch_reg.."%s+"..ss_reg.."%s+"..bss_reg.."%s+"..sec_reg.."%s+"..sig_reg.."%s+[0-9a-zA-Z/]+%s+%u+%s+%a+%s+"..wps_reg.."%s*")
+		if channel then
+			print(channel,ssid,bssid,security,signal,wps)
+            _file.write(channel..","..ssid..","..bssid..","..security..','..signal..','..wps)
 		end
 
 	end
