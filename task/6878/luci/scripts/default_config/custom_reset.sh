a636b5405efcb3278d828fae1f4ec2fdd22a435a
yue <yue@gmail.com>
2017-05-13 19:34:07 +0800

    update 6878.

--- task/6878/luci/scripts/default_config/custom_reset.sh
+++ task/6878/luci/scripts/default_config/custom_reset.sh
@@ -2,7 +2,9 @@
 
 [ -f "/usr/lib/lua/luci/scripts/default_config/wireless" ] && {
 	cp /usr/lib/lua/luci/scripts/default_config/wireless /etc/config/wireless
-	uci set wireless.wifi0.ssid="HomeULTERA_"`readwifimac | awk -F ':' '{print $4$5$6}' | tr '[a-f]' '[A-F]'`
-	uci set wireless.wifi0.key="`readwifimac | tr -d ':' | tr '[a-f]' '[A-F]'`"
+	ssid="HomeULTERA_`readwifimac | awk -F ':' '{print $4$5$6}' | tr '[a-f]' '[A-F]'`"
+	passwd="`echo -n $ssid | md5sum | awk -F '' '{print$1$3$5$7$9$11$13$15}' | tr '[a-f]' '[A-F]'`"
+	uci set wireless.wifi0.ssid=$ssid
+	uci set wireless.wifi0.key=$passwd
 	uci commit wireless
 }
