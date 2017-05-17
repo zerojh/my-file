a603231b9fadfc6a2614cd164b773c7618cb6710
yue <yue@gmail.com>
2017-05-17 14:57:48 +0800

    delete 6878

--- task/6878/luci/model/cbi/admin_system/led.lua
+++
@@ -1,9 +0,0 @@
-m = Map("oem", translate("System / LED"))
-
-s = m:section(NamedSection,"bluewave","brand",translate(""))
-s.addremove = false
-s.anonymous = true
-
-url = s:option(Value, "lte_url", translate("URL"))
-url.datatype="url"
-return m
