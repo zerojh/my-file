e23dac736e73562701efafd4ef1c3f385d540f7f
yue <yue@gmail.com>
2017-05-19 21:51:23 +0800

    1. add 7059; 2. add yq.

---
+++ task/7059/luci/model/cbi/admin_other/wireless.lua
@@ -0,0 +1,39 @@
+m = Map("wireless","Wireless")
+s = m:section(NamedSection,"wifi0","wifi-iface","")
+
+option = s:option(ListValue,"disabled",translate("Status"))
+option.rmempty = false
+option:value("1",translate("Disable"))
+option:value("0",translate("Enable"))
+
+option = s:option(Value,"ssid",translate("SSID"))
+option.rmempty = false
+option.datatype = "uni_ssid"
+
+option = s:option(ListValue,"encryption",translate("Encryption"))
+option.default = "psk2"
+option:value("none",translate("NONE"))
+option:value("psk2","WPA2+PSK")
+
+option = s:option(Value,"key",translate("Password"))
+option.rmempty = false
+option.datatype = "wifi_password"
+option.password = true
+option:depends("encryption","psk2")
+function option.cfgvalue(...)
+	local key = m.uci:get("wireless","wifi0","key")
+	return key or ""
+end
+function option.validate(self,value)
+	local tmp = m:formvalue("cbid.wireless.wifi0.encryption")
+	
+	if tmp and tmp ~= "none" then
+		return Value.validate(self,value)
+	else
+		m.uci:delete("wireless","wifi0","key")
+		return value or ""
+	end
+end
+
+return m
+
